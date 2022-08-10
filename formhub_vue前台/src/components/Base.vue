1c898ed520c473f43c0e989c096951acca535b51
Yang <lingyunyang@aaxischina.com>
2022-08-10 12:12:28 +0800

    add cicd

--- formhub_vue前台/src/components/Base.vue
+++ formhub_vue前台/src/components/Base.vue
@@ -24,7 +24,7 @@
               plain
               style="font-size: 15px;"
               @click="toLogin"
-              >登录</el-button
+              >登录8888888888</el-button
             >
             <el-button plain style="font-size: 15px;" @click="toRegiste"
               >注册</el-button
